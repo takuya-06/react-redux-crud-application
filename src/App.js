7240fe5a18860fffe11426d516c2f3ebdcfb6856
takuya-06 <t.furumi.dziosm06@gmail.com>
2021-10-21 23:37:55 +0900

    create class components and function component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render() {
+//class App extends Component {
+//    render() {
         //return <h1>Hello, world!</h1>;
-        return(
-            <React.Fragment>
-            <label htmlFor="bar">bar</label>
-            <input type="text" onChange={() => {console.log("I Clicked.")}} />
-            </React.Fragment>
-        )
+//        return(
+//            <React.Fragment>
+//            <label htmlFor="bar">bar</label>
+//            <input type="text" onChange={() => {console.log("I Clicked.")}} />
+//            </React.Fragment>
+//        )
         //return <input type="text" onChange={() => {console.log("I Clicked.")}} />;
-    }
+//    }
+//}
+
+const App = () => {
+    return(
+        <div>
+            <Cat />
+            <Cat />
+            <Cat />
+            <Cat />
+        </div>
+
+    ) 
+}
+
+const Cat = () => {
+    return <div>Meow!</div>
 }
 
 export default App;
